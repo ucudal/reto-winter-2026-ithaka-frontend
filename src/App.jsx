85a44be0fc5a1aa74b3eb58cd8f215b27292b627
Santiago Acosta <santiagon.acosta@correo.ucu.edu.uy>
2026-07-17 20:37:15 -0300

    Pagina 404 agregada, falta revisar diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import HelloWorld from './components/HelloWorld.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HelloWorld from "./components/HelloWorld.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
-  return <HelloWorld />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HelloWorld />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
