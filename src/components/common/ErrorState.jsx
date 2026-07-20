c1f880534e09882cc6adf1270ab539a9e6ab6110
Tiago <tiago.garrido@correo.ucu.edu.uy>
2026-07-20 02:03:24 -0300

    fix: remove comments and fix missing newline per review

--- src/components/common/ErrorState.jsx
+++ src/components/common/ErrorState.jsx
@@ -1,14 +1,5 @@
 import { Alert, AlertTitle, Box, Button } from '@mui/material'
 
-/**
- * Estado de error reutilizable: mensaje + boton de reintentar.
- * Pensado para usarse cuando falla el fetch de un listado o un detalle.
- *
- * @param {Object} props
- * @param {string} [props.title='Ocurrio un error']
- * @param {string} [props.message='No pudimos cargar la informacion. Intenta nuevamente.']
- * @param {() => void} [props.onRetry] - Si no se pasa, no se muestra el boton de reintentar
- */
 function ErrorState({
   title = 'Ocurrio un error',
   message = 'No pudimos cargar la informacion. Intenta nuevamente.',
@@ -32,5 +23,5 @@ function ErrorState({
     </Box>
   )
 }
-  
+
 export default ErrorState
