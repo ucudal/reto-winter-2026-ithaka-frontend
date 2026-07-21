7531fb85da1be5290d2b7821e8c6b3b355c0854a
Santiago Acosta <santiagon.acosta@correo.ucu.edu.uy>
2026-07-21 20:29:12 -0300

    bug fixed

--- src/pages/sections/Dashboard.jsx
+++ src/pages/sections/Dashboard.jsx
@@ -1,8 +1,6 @@
 import { Typography, Box } from "@mui/material";
 
 function Dashboard() {
-  throw new Error("Prueba del ErrorBoundary");
-
   return (
     <Box>
       <Typography variant="h4">Dashboard</Typography>
